6ec779ac7a14c989d71e1fc346fdd63531cd0de5
brad-duncan <17553852+brad-duncan@users.noreply.github.com>
2025-06-13 16:27:59 -0500

    Add files via upload

---
+++ Models/ProcessedAssemblyData.cs
@@ -0,0 +1,20 @@
+﻿using dnlib.DotNet;
+
+namespace rnDotnet.Models
+{
+    public class ProcessedAssemblyData
+    {
+        public ModuleDef Module { get; }
+        public string DecompiledCode { get; }
+        public string Sha256Hash { get; }
+        public string OriginalPath { get; }
+
+        public ProcessedAssemblyData(ModuleDef module, string decompiledCode, string sha256Hash, string originalPath)
+        {
+            Module = module;
+            DecompiledCode = decompiledCode;
+            Sha256Hash = sha256Hash;
+            OriginalPath = originalPath;
+        }
+    }
+}
