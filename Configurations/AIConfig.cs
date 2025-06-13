7dad0c770d3dbd28fb4ccd05fa6e3e444e66f5da
brad-duncan <17553852+brad-duncan@users.noreply.github.com>
2025-06-13 16:29:21 -0500

    Created AIConfig.cs

---
+++ Configurations/AIConfig.cs
@@ -0,0 +1,11 @@
+namespace rnDotnet.Configurations
+{
+    public class AIConfig
+    {
+        public string ProjectId { get; set; }
+        public string Location { get; set; }
+        public string Publisher { get; set; }
+        public string RenamingModel { get; set; }
+        public string SummaryModel { get; set; }
+    }
+}
