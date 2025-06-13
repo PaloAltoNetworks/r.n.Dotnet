1dac1c421f5be0befe5345eddd3e88caf0cd905f
brad-duncan <17553852+brad-duncan@users.noreply.github.com>
2025-06-13 16:26:19 -0500

    Add files via upload

---
+++ Services/UserInteraction/IConsoleInteraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace rnDotnet.Services.UserInteraction
+{
+    public interface IConsoleInteraction
+    {
+        string GetInput(string prompt);
+        bool Confirm(string message, bool defaultValue);
+        int GetMaxPasses(int recommendedPasses);
+        string GetSummaryPersona();
+        string GetRenamingPersona();
+        string PromptForExistingFileSelection(List<string> foundFiles);
+        void WriteLine(string message);
+        void Write(string message);
+    }
+}
